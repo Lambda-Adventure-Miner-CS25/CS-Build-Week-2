ad53b3a130f843358dac604c6a7f0f79f3780158
Ty Lippe <tylippe94@gmail.com>
2020-03-03 15:26:43 -0500

    working traversal and shopping

--- traversal/utils.py
+++ traversal/utils.py
@@ -12,3 +12,16 @@ class Stack():
         return len(self.stack)
     def print_stack(self):
         print(self.stack)
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
