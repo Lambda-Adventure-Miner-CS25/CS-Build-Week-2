c6c1290e6069ddb9dfa35c96de3d0feb5bdd519f
Ty Lippe <tylippe94@gmail.com>
2020-03-03 10:08:03 -0500

    Built a dict with all visited data, added movement file for bfs traversal
    Co-Authored-by: AndrewGoenner <drewgoenner@gmail.com>

---
+++ traversal/movement.py
@@ -0,0 +1,12 @@
+from visited import visited
+
+print(visited)
+
+# Import visited for wise movement 
+# Traversal that uses DFS, to find path 
+    # Store at room id 1
+    # Name Changer at 467
+    # Well at 55 
+    # Shrines at 461, 374
+    # Mine location (decoded coordinates as room id)
+# Add power abilites (STRETCH)
