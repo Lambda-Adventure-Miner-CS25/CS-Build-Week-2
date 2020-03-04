0c619161af56f9debab1a21cb4861dc3fa046b95
Min-Huang <minh.huang96@gmail.com>
2020-03-04 13:57:21 -0800

    fixing bugs

--- mining/auto_mining.py
+++ mining/auto_mining.py
@@ -53,20 +53,23 @@ if __name__ == '__main__':
         room_search(visited, current_location_id, well)
         
         # examine the well, and get the clue
+        print(f"looking for clue..........")
         clue = examine_well()[41:]
-             
+        print(clue)     
         # open ls8 file, and overwrite the clue on it
         f = open("mining/clue.ls8", "w")
         f.write(clue)
         f.close()
        
         # get the room id from ls8
-        print(f"looking for clue..........")
+        
         cpu = CPU()
         cpu.load()
         room = int("".join(cpu.run()[-3:]))
-        print("Clue: room",room)
-        
+        print("Clue: room", room)
+        # clean up the clue.ls8 file
+        open("mining/clue.ls8", "w").close()
+
         # go to the room
         print(f"going to room {room}..........")
         room_search(visited, well, room)
@@ -89,7 +92,7 @@ if __name__ == '__main__':
             print(data)
             time.sleep(data["cooldown"])
             
-            if 'New Block Forged' in data['messages']:
+            if 'messages' in data and 'New Block Forged' in data['messages']:
                 mined_coin += 1
                 print(f"Total Mined Coins in this time: {mined_coin}")
                 current_location_id = room
